ad35e03aaa77265d56d90f0980ce8d1e18b8830c
pbv <dev@pb.vision>
2023-12-30 12:08:00 -0800

    fix link inserted into jsdocs

--- docs/build.sh
+++ docs/build.sh
@@ -14,6 +14,6 @@ if [ "$GITHUB_SHA" = "" ]; then
 else
     gitHash=$GITHUB_SHA
 fi
-newLine="Generated from <a href=\"https:\\/\\/github.com\\/dound\\/fastify-firestore-service\\/tree\\/$gitHash\">$gitHash<\\/a><\\/article>"
+newLine="Generated from <a href=\"https:\\/\\/github.com\\/pbv-public\\/js-schema\\/tree\\/$gitHash\">$gitHash<\\/a><\\/article>"
 cat ./generated/index.html | sed -e "s/[<][/]article[>]/$newLine/g" > tmp
 mv tmp ./generated/index.html
