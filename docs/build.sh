ad35e8e2de5a2b1e433bb7ada3a6ccfcc2ca4ae4
pbv <dev@pb.vision>
2023-12-30 12:03:00 -0800

    add link in jsdoc to source it was generated from

--- docs/build.sh
+++ docs/build.sh
@@ -8,3 +8,12 @@ embed-markdown # Update .md files before embedding the .md files into docs
 cd "$script_path"
 rm -rf generated
 npx jsdoc --configure jsdoc.config.json `find ../src -name '*.js' -type f`
+
+if [ "$GITHUB_SHA" = "" ]; then
+    gitHash=`git rev-parse HEAD`
+else
+    gitHash=$GITHUB_SHA
+fi
+newLine="Generated from <a href=\"https:\\/\\/github.com\\/dound\\/fastify-firestore-service\\/tree\\/$gitHash\">$gitHash<\\/a><\\/article>"
+cat ./generated/index.html | sed -e "s/[<][/]article[>]/$newLine/g" > tmp
+mv tmp ./generated/index.html
